625ca1826af37d7708634fd2f088b299d35ccce8
Jayesh Geryani <jayeshgeryani13@gmail.com>
2024-12-12 18:11:48 +0530

    add subscription model

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -24,6 +24,6 @@ router.route("/login").post(loginUser)
 //secured routes
 
 router.route("/logout").post(verifyJwt, logoutUser)
-router.route("/rfresh-token").post(refreshAccessToken)
+router.route("/refresh-token").post(refreshAccessToken)
 
 export default router
